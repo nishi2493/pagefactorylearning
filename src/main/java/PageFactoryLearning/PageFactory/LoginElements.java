4e2ce8581a7d35f8fc6aa7399722df2e356443da
nishi <nishi.ajmani@gmail.com>
2025-10-31 19:09:34 +0530

    commied pagefactory project

---
+++ src/main/java/PageFactoryLearning/PageFactory/LoginElements.java
@@ -0,0 +1,46 @@
+package PageFactoryLearning.PageFactory;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import learn.pageobjects.LandingPage;
+
+public class LoginElements {
+
+	WebDriver driver;
+	
+	LoginElements(WebDriver d)
+	{
+		driver=d;
+		PageFactory.initElements(driver, this);
+	}
+	
+@FindBy(id="userEmail")
+WebElement username;
+
+
+@FindBy(id="userPassword")
+WebElement userPwd;
+
+@FindBy(id="login")
+WebElement submit;
+
+
+public void enterUsername(String uname)
+{
+username.sendKeys(uname);
+}
+
+public void enterPwd(String pwd)
+{
+	userPwd.sendKeys(pwd);
+}
+
+public void login()
+{
+submit.click();
+}
+
+}
---
+++ src/main/java/PageFactoryLearning/PageFactory/LoginTestCase.java
@@ -0,0 +1,32 @@
+package PageFactoryLearning.PageFactory;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+
+
+public class LoginTestCase {
+
+	public static void main(String args[])
+	{
+	WebDriver driver;
+	WebDriverManager.chromedriver().setup();
+	driver = new ChromeDriver();
+
+	LoginElements loginPg = new LoginElements(driver);
+	
+
+	driver.get("https://rahulshettyacademy.com/client");
+	driver.manage().window().maximize();
+	
+	loginPg.enterUsername("anshika@gmail.com");
+	loginPg.enterPwd("Iamking@000");
+	loginPg.login();
+	
+	}	
+}
+
+
+
